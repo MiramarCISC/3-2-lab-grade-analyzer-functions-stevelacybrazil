44b684a413bebe84b594d64ff08e4bc9ba2a1dd5
stevelacybrazil <75767576+stevelacybrazil@users.noreply.github.com>
2026-07-05 22:10:26 -0700

    Update grade_analyzer.cpp

--- src/grade_analyzer.cpp
+++ src/grade_analyzer.cpp
@@ -5,34 +5,42 @@ using namespace std;
 
 double calculateTotal(double score1, double score2, double score3) {
     // TODO: Return the sum of the three scores.
-    return 0.0;
+    return score1 + score2 + score3;
 }
 
 double calculateAverage(double total, int count) {
+    if (count <= 0) {
+        return 0.0;
+    }
+
+    return total / count;
+}
     // TODO:
     // If count is less than or equal to 0, return 0.0.
     // Otherwise, return total divided by count.
-    return 0.0;
-}
 
 char determineLetterGrade(double average) {
-    // TODO:
-    // Return 'A' for averages 90 or higher.
-    // Return 'B' for averages 80 or higher.
-    // Return 'C' for averages 70 or higher.
-    // Return 'D' for averages 60 or higher.
-    // Return 'F' otherwise.
-    return 'F';
+ if (average >= 90.0) {
+        return 'A';
+    } else if (average >= 80.0) {
+        return 'B';
+    } else if (average >= 70.0) {
+        return 'C';
+    } else if (average >= 60.0) {
+        return 'D';
+    } else {
+        return 'F';
+    }
 }
 
 bool isPassing(double average) {
     // TODO: Return true when average is 70 or higher.
-    return false;
+    return average >=70.0;
 }
 
 bool isValidScore(double score) {
     // TODO: Return true when score is between 0 and 100, inclusive.
-    return false;
+    return score >0.0 && score <= 100.0;
 }
 
 void printGradeReport(double total, double average, char letterGrade, bool passing) {
